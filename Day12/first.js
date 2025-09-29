eef2d991a5703426076dd8cdcc8267fd64621330
PurusottamParthasarathi <purusottamparthasarathi0104@gmail.com>
2025-09-29 22:42:05 +0530

    HTML and CSS class code

---
+++ Day12/first.js
@@ -0,0 +1,4 @@
+console.log("hello world")
+let a=5;
+let b=6;
+console.log(a+b);
---
+++ Day13/condition.js
@@ -0,0 +1,7 @@
+let age=17;
+if(age>=18){
+    console.log("you are eligible to vote");
+}
+else{
+    console.log("you are not eligible to vote");
+}
